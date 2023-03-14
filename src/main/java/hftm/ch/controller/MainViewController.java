8bea2b94d26c346ddec1f6c3dedbce36bd5fbea5
Richard Tafurth-Garcia <projects@cdstm.ch>
2023-03-14 15:17:00 +0100

    Controller skelleton

---
+++ src/main/java/hftm/ch/controller/MainViewController.java
@@ -0,0 +1,37 @@
+package hftm.ch.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.management.MXBean;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    MenuItem miPrint;
+
+    @FXML
+    MenuItem miQuit;
+
+    @FXML
+    TabPane tbMain;
+
+    @FXML
+    Tab tInhabitants;
+
+    @FXML
+    Tab tPlaces;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+    
+}
