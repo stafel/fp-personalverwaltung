d8e58e92c2245b5ad86d3c25d2e69c9a62b86cb2
leonardo.lang <leonardo.lang@hftm.ch>
2023-03-14 16:44:12 +0100

    Add App to launch scenes

---
+++ src/main/java/ch/hftm/App.java
@@ -0,0 +1,27 @@
+package ch.hftm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/places.fxml"));
+        // System.out.println(fxmlLoader.getLocation());
+        Scene scene = new Scene(fxmlLoader.load());
+        // Scene scene = new Scene(new FXMLLoader(App.class.getClassLoader().getResource("/view/places.fxml")).load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main( String[] args ) {
+        launch();
+    }
+}
